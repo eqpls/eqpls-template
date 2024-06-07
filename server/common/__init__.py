3941c9726a5333fad3af92e3e802b8166f3c65b3
jzide <jzide@equal.jz>
2024-06-07 18:39:43 +0900

    update

--- server/common/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-'''
-Equal Plus
-@author: Hye-Churn Jang
-'''
-
-from .constants import TimeString, EpException
-from .utils import setEnvironment, getConfig, Logger
-from .interfaces import TaskOperator, SyncRest, AsyncRest
